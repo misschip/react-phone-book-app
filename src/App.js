b67916866db1eeeb63ad421599c0d539684b5ef6
misschip <misschip@naver.com>
2020-08-06 06:47:19 +0900

    8/6

--- src/App.js
+++ src/App.js
@@ -2,13 +2,34 @@ import React, { Component } from 'react';
 import PhoneForm from './PhoneForm';
 
 class App extends Component {
-  hanleCreate = (data) => {
-    console.log(data);
+  id = 2
+  state = {
+    information: [
+      {
+        id: 0,
+        name: '김민준',
+        phone: '010-0000-0000'
+      },
+      {
+        id: 1,
+        name: '홍길동',
+        phone: '010-1111-1111'
+      }
+    ]
+  }
+
+  handleCreate = (data) => {
+    const { information } = this.state;
+    this.setState({
+      information: information.concat({id:this.id++, ...data})
+    })
   }
   render() {
+    const { information } = this.state;
     return (
       <div>
         <PhoneForm onCreate={this.handleCreate} />
+        { JSON.stringify(information) }
       </div>
     );
   }
